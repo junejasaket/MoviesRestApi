dc454d5e0f430e50523763609e9e701abc30629d
Saket Juneja <saket.juneja@levensystems.com>
2023-02-23 13:49:40 -0500

    Added Repositories and DI Abstraction

--- Movies.Api/Program.cs
+++ Movies.Api/Program.cs
@@ -1,3 +1,5 @@
+using Movies.Application;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,6 +7,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddApplication();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
